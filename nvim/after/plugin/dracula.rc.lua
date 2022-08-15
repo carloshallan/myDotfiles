ecba9c7990b2465934530cb90940c46e8f801f10
C.Hallan <38888731+carloshallan@users.noreply.github.com>
2022-08-15 15:53:13 -0300

    update config to neovim 7^

---
+++ nvim/after/plugin/dracula.rc.lua
@@ -0,0 +1,53 @@
+vim.cmd [[colorscheme dracula]]
+
+vim.g.dracula_colors = {
+  bg = "#282A3600",
+  fg = "#F8F8F2",
+  selection = "#44475A",
+  comment = "#6272A4",
+  red = "#FF5555",
+  orange = "#FFB86C",
+  yellow = "#F1FA8C",
+  green = "#50fa7b",
+  purple = "#BD93F9",
+  cyan = "#8BE9FD",
+  pink = "#FF79C6",
+  bright_red = "#FF6E6E",
+  bright_green = "#69FF94",
+  bright_yellow = "#FFFFA5",
+  bright_blue = "#D6ACFF",
+  bright_magenta = "#FF92DF",
+  bright_cyan = "#A4FFFF",
+  bright_white = "#FFFFFF",
+  menu = "#21222C",
+  visual = "#3E4452",
+  gutter_fg = "#4B5263",
+  nontext = "#3B4048",
+}
+
+-- show the '~' characters after the end of buffers
+vim.g.dracula_show_end_of_buffer = true
+-- use transparent background
+vim.g.dracula_transparent_bg = true
+-- set custom lualine background color
+vim.g.dracula_lualine_bg_color = "00000000"
+-- set italic comment
+vim.g.dracula_italic_comment = true
+-- set transparent vim bg
+vim.g.dracula_transparent_bg = true
+
+require("transparent").setup({
+  enable = true, -- boolean: enable transparent
+  extra_groups = { -- table/string: additional groups that should be cleared
+    -- In particular, when you set it to 'all', that means all available groups
+
+    -- example of akinsho/nvim-bufferline.lua
+    "BufferLineTabClose",
+    "BufferlineBufferSelected",
+    "BufferLineFill",
+    "BufferLineBackground",
+    "BufferLineSeparator",
+    "BufferLineIndicatorSelected",
+  },
+  exclude = {}, -- table: groups you don't want to clear
+})
